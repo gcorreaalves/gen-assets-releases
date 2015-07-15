f46a46cb86cebea92443f3213390b1a046a34013
Gustavo Corrêa Alves <gcajpa@gmail.com>
2015-07-15 00:10:21 -0300

    latest

--- generate.sh
+++ generate.sh
@@ -2,24 +2,32 @@
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 FILES="assets index.html"
+LATEST=""
+
+create_version(){
+  echo -e "Version - $2\n"
+
+  echo -e "Creating version directory: $2\n"
+  mkdir releases/$2
+
+  echo -e "Checkout to version: $2\n"
+  git checkout $1
+
+  echo -e "Copying the files to directory release\n"
+  cp -r $FILES releases/$2
+
+  echo -e "__________________________\n"
+}
 
 echo -e "Creating directories of releases\n"
 rm -r releases; mkdir releases
 
 for OUTPUT in $(git tag)
 do
-  echo -e "Version - $OUTPUT\n"
-
-  echo -e "Creating version directory: $OUTPUT\n"
-  mkdir releases/$OUTPUT
-
-  echo -e "Checkout to version: $OUTPUT\n"
-  git checkout $OUTPUT
-
-  echo -e "Copying the files to directory release\n"
-  cp -r $FILES releases/$OUTPUT
-
-  echo -e "__________________________\n"
+  create_version $OUTPUT $OUTPUT
+  LATEST=$OUTPUT
 done
+
+create_version $LATEST 'latest'
 
 git checkout $CURRENT_BRANCH
